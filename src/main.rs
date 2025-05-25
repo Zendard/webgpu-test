2b50b3c36fbd288cc4c82054a883b22192555cec
Zendard <thijsmuylaert@hotmail.com>
2025-05-25 19:26:07 +0200

    Testing GitUi

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod rendering;
 mod terrain;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    println!("Running... ");
     let event_loop = winit::event_loop::EventLoop::new().unwrap();
     let seed: u32 = std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
